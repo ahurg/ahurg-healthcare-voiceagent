95ebd271402001c3c7262da17e5dc1f336c9e40d
bhanushankar422 <bhanushankar422@gmail.com>
2025-05-17 23:11:31 -0500

    Initial setup of livekit project

---
+++ agent.py
@@ -0,0 +1,103 @@
+from dotenv import load_dotenv
+
+from livekit import agents, api
+from livekit.agents import (
+    AgentSession,
+    Agent,
+    RoomInputOptions,
+    function_tool,
+    get_job_context,
+    ChatContext,
+    BackgroundAudioPlayer,
+    AudioConfig,
+    BuiltinAudioClip
+)
+from livekit.plugins import (
+    openai,
+    noise_cancellation,
+    silero,
+)
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+load_dotenv()
+
+
+class ConsentCollector(Agent):
+    def __init__(self):
+        super().__init__(
+            instructions="""Your are a voice AI agent with the singular task to collect positive 
+            recording consent from the user. If consent is not given, you must end the call."""
+        )
+
+    async def on_enter(self) -> None:
+        await self.session.say("May I record this call for quality assurance purposes?")
+
+    @function_tool()
+    async def on_consent_given(self):
+        """Use this tool to indicate that consent has been given and the call may proceed."""
+
+        # Perform a handoff, immediately transfering control to the new agent
+        return HelpfulAssistant(chat_ctx=self.session._chat_ctx)
+
+    @function_tool()
+    async def end_call(self) -> None:
+        """Use this tool to indicate that consent has not been given and the call should end."""
+        await self.session.say("Thank you for your time, have a wonderful day.")
+        job_ctx = get_job_context()
+        print(f"Deleting room {job_ctx.room.name}")
+        await job_ctx.api.room.delete_room(api.DeleteRoomRequest(room=job_ctx.room.name))
+
+
+class HelpfulAssistant(Agent):
+    def __init__(self, chat_ctx: ChatContext) -> None:
+        super().__init__(
+            instructions="You are a helpful voice AI assistant. Your name is Bhanu.",
+            chat_ctx=chat_ctx
+        )
+
+    async def on_enter(self) -> None:
+        await self.session.say("Hello, how can I help you today?")
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession(
+        stt=openai.STT(model="gpt-4o-mini-transcribe"),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=openai.TTS(model="gpt-4o-mini-tts", voice="ash"),
+        vad=silero.VAD.load(),
+        turn_detection=MultilingualModel(),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=ConsentCollector(),
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
+
+    background_audio = BackgroundAudioPlayer(
+        # play office ambience sound looping in the background
+        ambient_sound=AudioConfig(
+            BuiltinAudioClip.OFFICE_AMBIENCE, volume=0.8),
+        # play keyboard typing sound when the agent is thinking
+        thinking_sound=[
+            AudioConfig(BuiltinAudioClip.KEYBOARD_TYPING, volume=0.8),
+            AudioConfig(BuiltinAudioClip.KEYBOARD_TYPING2, volume=0.7),
+        ],
+    )
+
+    await background_audio.start(room=ctx.room, agent_session=session)
+
+    await ctx.connect()
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
